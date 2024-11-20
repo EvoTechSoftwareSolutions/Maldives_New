361213a1b86801b1375a46e69765ea5680fb5e72
Kawya Awarjana <kawya.evotech@gmail.com>
2024-11-21 02:24:59 +0530

    Kawya

--- home/index.php
+++ home/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>dodo Travel</title>
+    <title>dodo.lk</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <!-- <meta content="" name="keywords">
     <meta content="" name="description"> -->
